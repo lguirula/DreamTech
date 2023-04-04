22b89c9fc9ada98a52179a58bedabd86b03dece0
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-04-04 11:18:12 -0300

    Cambio de instrucción con timers

--- main.py
+++ main.py
@@ -6,49 +6,56 @@ from PyQt5.QtCore import QPropertyAnimation,QEasingCurve
 from PyQt5 import QtWidgets,QtCore
 from PyQt5.uic import loadUi
 import json
-import time    #para delays
+import time # para delays
 import threading
 
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
             super(VentanaPrincipal,self).__init__()
-            loadUi('test.ui',self)  #uso el diseño sin necesitar el codigo del diseño
-            #arranca con la pantalla de consignas
+            loadUi('test.ui',self)  # Uso el diseño sin necesitar el codigo
+            # Arranca con la pantalla de consignas
             self.stackedWidget.setCurrentWidget(self.consigna) 
+            # Conexion de Botones
+            self.bt_start.clicked.connect(lambda: self.recordWords())
             self.bt_start.hide()
-            
-           #Funciones de Labels
-            #self.instrucciones()
-            #self.start()
-            self.t = threading.Timer(0.5, self.instrucciones)
-            self.t.start()
-            #self.mostrar()
-            self.t2 = threading.Timer(10, self.mostrar)
-            self.t2.start()
-            #Conexion de Botones
-            self.bt_start.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.palabra))
+            # Muestra instrucciones
+            self.instructions()
 
-    
-
-    def instrucciones(self):
+    def instructions(self):
         with open('Instrucciones.txt') as inst:
             text = inst.read()
-        labels = text.split('#')
-        print(labels)
-        self.lb_consigna.setText(str(labels[0]) )
-        time.sleep(2)
-        self.lb_consigna.setText(str(labels[1]) )
-        time.sleep(2)
-        self.lb_consigna.setText(str(labels[2]) )
-        self.bt_start.show()
+        self.inst_labels = text.split('#')
+        self.lb_consigna.setText(self.inst_labels[0])
+        self.idx_labels = 1
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateInstructions)
+        self.timer.start(2*1000)
 
-    def mostrar(self):
+    def updateInstructions(self):
+        if self.idx_labels >= len(self.inst_labels):
+            self.bt_start.show()
+            self.timer.stop()
+        else:
+            self.lb_consigna.setText(self.inst_labels[self.idx_labels])
+            self.idx_labels += 1
+
+    def recordWords(self):
+        self.stackedWidget.setCurrentWidget(self.palabra)
         with open("data.json","r") as f:
            data = json.load(f)
+        self.word_labels = data['palabra']
+        self.lb_palabra.setText(self.word_labels[0])
+        self.idx_words = 1
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateWords)
+        self.timer.start(4*1000)
 
-        for i in range (len(data['palabra'])):
-            self.lb_palabra.setText(data['palabra'][i])
-            time.sleep(2.5)
+    def updateWords(self):
+        if self.idx_words >= len(self.word_labels):
+            self.timer.stop()
+        else:
+            self.lb_palabra.setText(self.word_labels[self.idx_words])
+            self.idx_words += 1
 
     '''
     def start(self):
@@ -57,8 +64,6 @@ class VentanaPrincipal(QMainWindow):
         #self.timerA.timeout.connect(self.pamitomi)
         self.timerA.start(10)
     '''
-
-        
 
 
 if __name__ == "__main__":
